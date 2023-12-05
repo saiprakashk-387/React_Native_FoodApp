0a523f42893074ff03ce928dd782dff248f1db4c
Sai Prakash k <prakash387@yahoo.com>
2023-12-05 15:11:20 +0530

    getallfoodrooute

--- Server/Routes/GetFood.js
+++ Server/Routes/GetFood.js
@@ -12,5 +12,15 @@ router.post("/food", async (req, res) => {
     res.json({ err: err });
   }
 });
+router.get("/foodlist", async (req, res) => {
+  try {
+    // const foodtype = req.body.foodtype;
+    // const availabledate = req.body.availabledate;
+    const getFoodlist = await breakFast.find({});
+    res.status(200).json(getFoodlist);
+  } catch (err) {
+    res.json({ err: err });
+  }
+});
 
 module.exports = router;
